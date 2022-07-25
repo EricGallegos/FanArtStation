570e32860732d454e76830aaaf1a7365b978659d
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-07-25 16:12:51 -0600

    new info and delete api

--- index.js
+++ index.js
@@ -29,13 +29,23 @@ let art = [
   }
 ];
 
+// HOME //
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 })
 
+// Info API //
+// GET
+app.get('/api/info', (request, response) =>{
+  let time = new Date();
+  response.send(`<p>FanArtStation has ${art.length} badass pieces of art</p> <p>${time} </p>`);
+});
+
+// Art Collection API //
+// GET
 app.get('/api/art', (request, response) => {
   response.json(art);
-})
+});
 
 app.get('/api/art/:id', (request, response) => {
   const id = Number(request.params.id);
@@ -48,6 +58,27 @@ app.get('/api/art/:id', (request, response) => {
   }
 });
 
+// POST
+app.post('/api/art', (request, response) => {
+  const body = request.body;
+  console.log(request.body);
+  if(!body.url || !body.ip || !body["media-type"]){
+    return response.status(400).json({ error: 'content missing' });
+  }
+
+  const piece = {
+    id: generateID(),
+    url: body.url,
+    date: new Date(),
+    "media-type": body["media-type"],
+    tags: body.tags || [],
+    ip: body.ip
+  }
+  art = art.concat(piece);
+  response.json(piece);
+});
+
+// DELETE
 app.delete('/api/art/:id', (request, response) =>{
   const id = Number(request.params.id);
   art = art.filter(art => art.id !== id);
@@ -55,25 +86,11 @@ app.delete('/api/art/:id', (request, response) =>{
   response.status(204).end();
 });
 
-app.post('/api/art', (request, response) => {
-  const body = request.body;
-  console.log(request.body);
-  if(!body.url){
-    return response.status(400).json({ error: 'content missing' });
-  }
-
-  const piece = {
-    id: generateID(),
-    url: body.url,
-    date: generateDate(),
-    "media-type": body["media-type"],
-    tags: body.tags,
-    ip: body.ip
-  }
-  art = art.concat(piece);
-
-  console.log(art);
-  response.json(piece);
+app.delete('/api/art/delete/:url', (request, response) =>{
+  const url = request.params.url;
+  art = art.filter(art => art.url !== url);
+  console.log('Deleted: ', url)
+  response.status(204).end();
 });
 
 const PORT = 3001;
@@ -83,11 +100,7 @@ console.log(`server is running on ${PORT}`);
 
 const generateID = () => {
   const maxID = art.length > 0
-    ? Math.max(...art.map(n => n.id))
+    ? Math.max(...art.map(n => n.id)) //Find largest id # in collection
     : 0
   return maxID + 1;
-}
-
-const generateDate = () => {
-  return new Date();
 }
