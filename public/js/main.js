d3f4559a51e3abefc1fb9742cf71ff018da74173
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-07-27 15:10:14 -0600

    frontend JS started & style changes

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+let splashXClicked = false;
+const splashes = Array.from(document.querySelectorAll('.imageSplash'));
+const exitSplashButtons = Array.from(document.querySelectorAll('.imageSplash .splashHeader button'));
+
+splashes.forEach( splash => {
+  splash.addEventListener('click', toggleActiveSplash);
+})
+exitSplashButtons.forEach( button => {
+  button.addEventListener('click', setSplashInactive);
+});
+
+
+function toggleActiveSplash(){
+  for( let i in splashes ){
+    // If this was the splash clicked
+    if( !splashXClicked && this.getAttribute('name') === splashes[i].getAttribute('name') ) {
+      this.setAttribute("style", "border-color: purple; overflow: scroll;");
+
+      let title = document.querySelector(`#${this.getAttribute('name')}`);
+      title.setAttribute("style", "color: white;")
+
+      let button = document.querySelector(`#${this.getAttribute('name')} + button`)
+      button.setAttribute("style", "opacity: .7;")
+    }
+
+    // For all splashes that werent clicked
+    if( splashXClicked || this.getAttribute('name') != splashes[i].getAttribute('name') ){
+      splashes[i].setAttribute("style", "border-color: grey; overflow: hidden;");
+
+      let title = document.querySelector(`#${splashes[i].getAttribute('name')}`);
+      title.setAttribute("style", "color: grey;")
+
+      let button = document.querySelector(`#${splashes[i].getAttribute('name')} + button`)
+      button.setAttribute("style", "opacity: 0;")
+    }
+  }
+  splashXClicked = false;
+}
+
+function setSplashInactive(){
+  splashXClicked = true;
+}
