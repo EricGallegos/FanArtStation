ac23a5c85307b1255b5cfdf128f5fde2856bb708
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-07-28 13:28:36 -0600

    DB functionality and ejs modularity

--- server.js
+++ server.js
@@ -5,41 +5,58 @@ require('dotenv').config();
 
 const connectionString = process.env.DB_STRING;
 let db, mediaCollection, dbName = 'CreationStation';
+collectionName = 'Media'
+let splashes = [];
+let numOfSplashes = 6;
+let splashNames = ['Latest', 'Hot', 'Top this week', 'All Timers', 'Up and Coming', 'May have Missed']
 
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+// MongoDB Maintenance //
+
 mongo.connect(connectionString)
   .then(client => {
     console.log('Connected to MongoDB');
     db = client.db(dbName);
-    mediaCollection = db.collection('Media');
-
-    const cursor = mediaCollection.find().toArray()
-      .then( collection => {
-        collection.forEach( entry => {
-            firstSplash.push(entry);
-        })
-      }).catch(error => console.log(error));
+    mediaCollection = db.collection(collectionName);
+    updateSplashes();
 
 }).catch(error => console.log(error));
 
+function updateSplashes(){
+  for( let i = 0; i < numOfSplashes; i++){
+    splashes[i] = [];
+    updateSplash(splashNames[i], i);
+    if(i % 2 == 1) splashes[i].reverse();
+  }
+}
+function updateSplash(splashName, i){
+  const cursor = mediaCollection.find({}).toArray()
+    .then( collection => {
+      collection.forEach( document => {
+        splashes[i].push(document);
+      })
+    })
+    .catch(error => console.log(error));
+}
+
+// REST API's ///////////////////////////////////////////////////////////////
+// GET
 // HOME //
 app.get('/', (request, response) => {
-
-  response.render('index', {firstSplash});
+  response.render('index', {splashes, splashNames});
 })
 
+
 // Info API //
-// GET
 app.get('/api/info', (request, response) =>{
   let time = new Date();
   response.send(`<p>FanArtStation has ${firstSplash.length} badass pieces of art</p> <p>${time} </p>`);
 });
 
 // Art Collection API //
-// GET
 app.get('/api/art', (request, response) => {
   response.json(art);
 });
@@ -119,9 +136,9 @@ app.delete('/api/art/delete/:url', (request, response) =>{
 // Class containing all information pertaining to any single piece of fanart //
 class Media{
   constructor( {name, url, creator, date, uploadedBy, accountURL} ){
-    this.name = name || "KDA Evelynn";
+    this.name = name || "Trippy Smith";
     this.url = url || "#";
-    this.creator = creator || 'John Snoe';
+    this.creator = creator || 'Igrit';
     this.date = new Date();
     this.uploadedBy = uploadedBy;
     this.upvotes = 0;
@@ -131,22 +148,23 @@ class Media{
 
 
 const creations = [
-  { url: 'https://cdna.artstation.com/p/assets/images/images/014/649/132/large/parzival-kda-evelyym.jpg?1544834412' },
-  { url: 'https://cdnb.artstation.com/p/assets/images/images/008/666/799/large/andres-blanco-dvafanartsss-ahora-si-final.jpg?1514408536' },
-  { url: 'https://external-preview.redd.it/LGiMRW0B-Mz2KzTORYU-w2gp93R2Q5a7C4F1fDLkhiA.png?auto=webp&s=962911c344ecc4d6629b356ea42b60cfe88bfe02' },
-  { url: 'http://www.themanwiththehat.de/files/attachments/pZZ7xT1g.jpg' },
-  { url: 'https://i.pinimg.com/736x/9a/ea/99/9aea99d61bfd50a9d6aadb4469be33a4.jpg' },
+  { url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRTEGVDaMqzITzzmUQBjK6nB3F8EoFrfqkrQ&usqp=CAU' },
+  { url: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/fanart-of-jinx-natasza-remesz.jpg' },
+  { url: 'https://i.pinimg.com/736x/05/4c/e1/054ce148c5b1c730594096ccb0612080.jpg' },
+  { url: 'https://www.sproutwired.com/final-fantasy-the-series-heroines-shine-in-beautiful-fan-art/' },
+  { url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnu1RMFjPopRbCD69CeMPQWRoPyniNFY7Tfw&usqp=CAU' },
   { url: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/e72b3157750847.59e232905123e.jpg'},
-  { url: 'https://external-preview.redd.it/3czaaDd4GSc-0-3gk8VTDi4yYdYjLrxln-iGvSLUQ4o.jpg?auto=webp&s=ad9739951fe88186d17784138fcc4a9d6fa98060'},
-  { url: 'https://assets.wallpapersin4k.org/uploads/2017/04/Wallpaper-Fan-Art-21.jpg'},
-  { url: 'http://pm1.narvii.com/7319/87253137c79924ac8bef849df9ab76369318c316r1-2048-2047v2_uhq.jpg'},
-  { url: 'https://static.wikia.nocookie.net/f8c3ae14-b1be-4dea-a0e0-19c40868845c/scale-to-width/755'},
-  { url: 'https://cdna.artstation.com/p/assets/images/images/001/207/916/large/oreki-genya-x.jpg?1442227975'},
-  { url: 'https://cdnb.artstation.com/p/assets/images/images/014/607/823/large/aj-ramos-goblin-slayer-final-2.jpg?1544677847'},
-  { url: 'https://cdna.artstation.com/p/assets/images/images/016/614/660/large/femke-brouwer-pikachu10.jpg?1552833465'},
+  { url: 'https://cdna.artstation.com/p/assets/images/images/036/818/886/large/kyle-martin-mileena-splash-v4.jpg?1618707428'},
+  { url: 'https://testyourmight.com/data/avatars/h/6/6457.jpg?1563600559'},
+  { url: 'https://c4.wallpaperflare.com/wallpaper/1/456/840/lol-league-of-legends-fan-art-fiora-wallpaper-preview.jpg'},
+  { url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9U_bsQfwomVbr_d8CREON8_9zovilKcY88g&usqp=CAU'},
+  { url: 'https://pbs.twimg.com/media/E8Q9OZmXsAA4RZt?format=jpg&name=large'},
+  { url: 'https://lh3.googleusercontent.com/yvbSoZUtXNMPVckIE3MAuSaT5vkz1wSWj0br7SUUYtM3RSgSBIWKHiX-SJNUto5lsVWnfvdPFQc9GguXuViCNfv6uN7Az-W_EksqLlk=w600'},
+  { url: 'https://i.pinimg.com/originals/a0/ed/d6/a0edd65a5343f0e972157b5c7cbbb0ef.jpg'},
 ]
 let testMedia = []
 let firstSplash = []
+
 
 function populateTestMedia(){
   for(let i = 0; i < 13; i++){
